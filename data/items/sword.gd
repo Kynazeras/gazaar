6088508cc1c49f4357f9217030acb9dcd94d777f
Seth Holloway <sethholloway@Jasmines-MacBook-Pro.local>
2025-03-28 07:16:43 -0400

    assets(sprites): Adding free assets back to project

--- data/items/sword.gd
+++ data/items/sword.gd
@@ -3,8 +3,7 @@ extends Item
 func apply_effects(targets: Array[Node]) -> void:
 	var damage_effect := DamageEffect.new()
 	var shield_effect := ShieldEffect.new()
-	damage_effect.amount = 10
+	damage_effect.amount = 20
 	shield_effect.amount = 10
-	print('dealing 10 damage')
 	damage_effect.execute(targets)
 	shield_effect.execute(targets)
