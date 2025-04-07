8437eb1337ad0f4f7ba04b708c9a4489946229fb
Seth Holloway <the.seth.holloway@gmail.com>
2025-04-06 22:08:06 -0400

    feat(item_ui): Adds dynamic sizing of items

--- components/drag_and_drop.gd
+++ components/drag_and_drop.gd
@@ -11,51 +11,49 @@ signal drag_cancelled
 @export var freedom: DRAG_FREEDOM = DRAG_FREEDOM.FREE
 
 var dragging := false
-# var original_parent: Control = null
 
 func _ready() -> void:
-    # print(target.get_rect().has_point(Vector2(700,700)))
-    target.gui_input.connect(_on_target_gui_input)
+	target.gui_input.connect(_on_target_gui_input)
 
 
 func _process(_delta: float) -> void:
-    if dragging and target:
-        match freedom:
-            DRAG_FREEDOM.FREE:
-                target.global_position = target.get_global_mouse_position() - Vector2.ZERO
-            DRAG_FREEDOM.X_LOCKED:
-                target.global_position.x = target.get_global_mouse_position().x - Vector2.ZERO.x
-            DRAG_FREEDOM.Y_LOCKED:
-                target.global_position.y = target.get_global_mouse_position().y - Vector2.ZERO.y
-            _:
-                target.global_position = target.get_global_mouse_position() - Vector2.ZERO
-        
+	if dragging and target:
+		match freedom:
+			DRAG_FREEDOM.FREE:
+				target.global_position = target.get_global_mouse_position() - (Vector2.ZERO + target.pivot_offset)
+			DRAG_FREEDOM.X_LOCKED:
+				target.global_position.x = target.get_global_mouse_position().x - (Vector2.ZERO.x + target.pivot_offset.x)
+			DRAG_FREEDOM.Y_LOCKED:
+				target.global_position.y = target.get_global_mouse_position().y - (Vector2.ZERO.y + target.pivot_offset.y)
+			_:
+				target.global_position = target.get_global_mouse_position() - (Vector2.ZERO + target.pivot_offset)
+		
 
 
 func _input(event: InputEvent) -> void:
-    if dragging and event.is_action_released('select'):
-        _drop()
-    if dragging and event.is_action_pressed('cancel'):
-        _cancel_drag()
+	if dragging and event.is_action_released('select'):
+		_drop()
+	if dragging and event.is_action_pressed('cancel'):
+		_cancel_drag()
 
 
 func _start_drag():
-    drag_started.emit()
-    dragging = true
-    target.z_index = 99
+	drag_started.emit()
+	dragging = true
+	target.z_index = 99
 
 
 func _drop():
-    dropped.emit(target)
-    dragging = false
+	dropped.emit(target)
+	dragging = false
 
 
 func _cancel_drag():
-    drag_cancelled.emit()
-    dragging = false
-    target.z_index = 0
+	drag_cancelled.emit()
+	dragging = false
+	target.z_index = 0
 
 
 func _on_target_gui_input(event: InputEvent):
-    if not dragging and event.is_action_pressed("select"):
-        _start_drag()
+	if not dragging and event.is_action_pressed("select"):
+		_start_drag()
